b3e2ede0bfa04bf246a3515cf11f68c6a73382d4
mortem-000 <unik810dhungel@gmail.com>
2025-01-21 09:28:25 +0545

    modules

--- basics/day5.js
+++ basics/day5.js
@@ -25,24 +25,56 @@
 // console.log('Last');
 
 //async/await
-function fetchData(){
-    return new Promise((resolve,reject)=>{
-        setTimeout(()=>{
-            const success = true;
-            if (success){
-                resolve('Data fetched');
-            }
-            else{
-                reject('Error occured');
-            }
-            },2000);
-    });
-}
+// function fetchData(){
+//     return new Promise((resolve,reject)=>{
+//         setTimeout(()=>{
+//             const success = true;
+//             if (success){
+//                 resolve('Data fetched');
+//             }
+//             else{
+//                 reject('Error occured');
+//             }
+//             },2000);
+//     });
+// }
 
-async function displayData(){
-    console.log('Initial');
-    const data = await fetchData();
-    console.log(data);
-    console.log('Final');
-}
-displayData();
+// async function displayData(){
+//     console.log('Initial');
+//     const data = await fetchData();
+//     console.log(data);
+//     console.log('Final');
+// }
+// displayData();
+
+//path module
+
+// const path = require('path');
+
+// const file = 'C:/Users/me/Desktop/Nodejs/basics/day5.js';
+// console.log(path.basename(file));
+// console.log(path.dirname(file));
+// console.log(path.extname(file));
+
+
+//fs module
+//read filr 
+const fs = require('fs');
+
+// fs.readFile('day5.js','utf8',(err,data)=>{
+//     if (err){
+//         console.log('Error:',err);
+//     }
+//     else{
+//         console.log(data);
+//     }
+// });
+
+//write file
+
+// fs.writeFile('git.txt','console.log("Hello")',(err)=>{
+//     if (err){
+//         console.log('Error:',err);
+//     }
+// });
+
