8333ac528275d4f7516f730659fc503fd6a1231f
mortem-000 <unik810dhungel@gmail.com>
2025-01-21 08:56:21 +0545

    standard file management

--- basics/day5.js
+++ basics/day5.js
@@ -18,10 +18,31 @@
 // console.log('End');
 
 //asynchronous programming in javascript
-console.log('Start');
-setTimeout(() => {
-    console.log('Time Out Completed');
-},3000);
-console.log('Last');
+// console.log('Start');
+// setTimeout(() => {
+//     console.log('Time Out Completed');
+// },3000);
+// console.log('Last');
 
 //async/await
+function fetchData(){
+    return new Promise((resolve,reject)=>{
+        setTimeout(()=>{
+            const success = true;
+            if (success){
+                resolve('Data fetched');
+            }
+            else{
+                reject('Error occured');
+            }
+            },2000);
+    });
+}
+
+async function displayData(){
+    console.log('Initial');
+    const data = await fetchData();
+    console.log(data);
+    console.log('Final');
+}
+displayData();
