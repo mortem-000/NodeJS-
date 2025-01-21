a6b028feed839f973893fe2b6917c991ce712fd6
mortem-000 <unik810dhungel@gmail.com>
2025-01-21 08:58:16 +0545

    First task

---
+++ tasks/task1.js
@@ -0,0 +1,47 @@
+//calculator function take two no from user and operator as argument and return the result.(using switch case)
+
+// function calculator(a,b,operator){
+//     switch(operator){
+//         case '+':
+//             console.log('Addition:',a+b);
+//             break;
+//         case '-':
+//             console.log('Subtraction',a-b);
+//             break;
+//         case '*':
+//             console.log('Multiplication:',a*b);
+//             break;
+//         case '/':
+//             console.log('Division:',a/b);
+//             break;
+//         default:
+//             return 'Invalid operator';
+//     }
+// }
+// let a= 10;
+// let b= 20;
+// let operator= '+';
+// calculator(a,b,operator);
+
+// for (i=0;i<=50;i+=2){
+//     console.log(i);
+// }
+
+// function largest(arr){
+//     let max= arr[0];
+//     for (let i of arr){
+//         if (i>max){
+//             max=i;
+//         }
+//     }
+//     return max;
+// }
+// console.log(largest([10,20,30,40,50]));
+
+
+// let num =[1,2,3,4,5,6,7,8,9,10];
+
+// let squares= num.filter((x)=>{
+//     return x%2===0;
+// })
+// console.log(squares);
