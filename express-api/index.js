89ed32ba2df3e437968e06aae91a7988d22b8dc4
mortem-000 <unik810dhungel@gmail.com>
2025-01-23 09:41:15 +0545

    middlewares

--- express-api/index.js
+++ express-api/index.js
@@ -1,36 +1,114 @@
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
+import express from "express";
+import morgan from "morgan"
+import cors from "cors"
 
 const app = express()
 
 app.use(express.json())
 
-app.get('/test', (req, res) => {
-    res.end('Response from test endpoint')
+
+app.use(morgan("dev"))
+// app.use(cors())
+
+
+
+const corsOptions = {
+    origin: '*', // Allow only requests from this origin
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
+
+app.use((req, res, next)=>{
+    console.log("This is applicaiton level middleware")
+    next();
 })
 
-app.get('/',(req,res)=>{
-    res.end('Response from main endpoint')
+//authentication
+app.use((req, res, next)=>{
+    let isLoggedIn = true;
+    if(isLoggedIn){
+        next();
+    }else{
+        res.end("I have access to response cycle.")
+    }
 })
 
-
-app.post('/login', (req, res) => {
-    //login code
-    const { username, password } = req.body
-    console.log('username:', username)
-    console.log('password:', password)
-    //email exist 
-    //password match
-    //generate token=>return
-    res.end('Response from login endpoint')
+//authorization
+app.use((req, res, next)=>{
+    let isAdmin = true;
+    if(isAdmin){
+        next();
+    }else{
+        res.end("Only admins are allowded to enter")
+    }
 })
 
+//middlewares have access to req.body ko data
+app.use(function (req,res,next){
+    const data = req.body
+    console.log("data: ", data)
+    req.body = {
+        ...req.body,
+        age: 18
+    }
+    next();
+})
 
+app.get("/test",(req,res)=>{
+    res.end("Response from test endpoint")
+})
 
-app.listen(3005, (err) => {
-    if (err)
-        console.log('Error: ', err)
-    else
-        console.log('Server is running on port 3005')
+app.get("/",(req,res)=>{
+    res.end("response from main endpoint")
+})
+
+app.post("/",(req,res)=>{
+    res.end("response from post request")
+})
+
+app.post("/login",(req,res)=>{
+    console.log("req=> body at login : ", req.body)
+    res.end("response from login")
+})
+
+// app.post("/login",(req,res)=>{
+//     const{email, password} =req.body
+//     //email exist
+//     //password correct
+//     //token generate => return 
+
+//     res.end("From Login")
+// })
+
+// app.get("/student",()=>{
+//     //get token
+//     //token decrypt
+//     //
+
+//     //database => get info
+//     //sanitize info
+//     // return info
+// })
+
+// const router = express.Router();
+
+// // Router-level middleware
+// router.use((req, res, next) => {
+//   console.log('Router-level middleware executed');
+//   next();
+// });
+
+// router.get('/test', (req, res) => {
+//   res.send('Router-level middleware example');
+// });
+
+// app.use('/api', router); // Mount the router on the '/api' path
+
+app.listen(3005,(err)=>{
+    if(err){
+        console.log("Error running the server: ", err);
+    }
+    console.log("Server is listening at port 3005")
 })
